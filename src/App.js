9864304d0c7462437ef77de86ea4d87e07d4d178
lee <winertop6924@naver.com>
2021-01-31 17:41:40 +0900

    link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react'
-import MyComponent from './MyComponent'
-const App =()=>{
-  return <MyComponent>세제귀모링</MyComponent>
-}
-
+/*https://github.com/winertop6924/practice.gitS */
+import React from "react";
+import MyComponent from "./MyComponent";
+const App = () => {
+  return <MyComponent>세제귀모링</MyComponent>;
+};
 
 export default App;
-
