4b824ee39741b70a39faccc2fc2824d04d548e62
lee <winertop6924@naver.com>
2021-02-01 16:33:08 +0900

    컴포넌트 props

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@
 import React from "react";
 import MyComponent from "./MyComponent";
 const App = () => {
-  return <MyComponent>세제귀모링</MyComponent>;
+  return (
+    <MyComponent name="아오" 좋아하는숫자={2}>
+      세제귀모링
+    </MyComponent>
+  );
 };
+/*src\MyComponent.js
+  Line 15:13:  Typo in static class property declaration  react/no-typos
 
+Search for the keywords to learn more about each error.
+오류 해결-> name을 바꿀것 바로해결됨 */
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import './App.css'
-const MyComponent = ({name,children}) =>  {
-
-return(
-    <div className ='react'>
-        반갑노 이기 내이름은 {name}이다 이기야 <br/>
-        children 값은 {children} 이다 이기
+import React from "react";
+import "./App.css";
+import PropTypes from "prop-types";
+const MyComponent = ({ name, 좋아하는숫자, children }) => {
+  return (
+    <div className="react">
+      반갑구마잉 내이름은 {name}이지라 <br />
+      children 값은 {children} 이다잉 -광주ver-
+      <br />
+      제가 좋아하는 숫자는 {좋아하는숫자}입니다.
     </div>
-);
+  );
 };
-MyComponent.defaultProps ={
-    name: '세제귀동'
+MyComponent.defaultProps = {
+  name: "세제귀동",
+};
+MyComponent.PropTypes = {
+  name: PropTypes.string,
+  좋아하는숫자: PropTypes.number.isRequired,
 };
 
 export default MyComponent;
