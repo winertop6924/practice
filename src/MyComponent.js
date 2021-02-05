7e271889adb5875c829db06b40132cb50793ef64
lkk <winertop6924@naver.com>
2021-02-05 19:26:26 +0900

    state값에 여러개 넣기

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -7,17 +7,19 @@ class MyComponent extends Component {
     //state의 초깃값 설정
     this.state = {
       number: 0,
+      fixedNumber:0
     };
   }
   render() {
-    const { number } = this.state; //state를 조회할때 this.state로 조회
+    const { number,fixedNumber } = this.state; //state를 조회할때 this.state로 조회
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           //onclick을 통해 버튼이클릭됬을때 호출할 함수 저장
           onClick={() => {
-            this.setState({ number: number + 1 });
+            this.setState({ number: number + 1 ,fixedNumber: number+2});
           }}
         >
           +1
